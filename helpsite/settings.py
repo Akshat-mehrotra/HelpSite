8247e7e39e3008aaf41c98fb9593a12bab2cd79e
Akshat Mehrotra <akshatmehrotra2004@gmail.com>
2022-06-01 00:24:16 +0530

    Made production state revert to dev state if DEBUG is not set

--- helpsite/settings.py
+++ helpsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from distutils.log import debug
 from pathlib import Path
 import os
 
@@ -21,15 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY = os.environ["SECRET_KEY"]
-except KeyError as e:
-    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    with open('./secret_key', 'r') as f:
+        SECRET_KEY = f.read()
+
+if not SECRET_KEY:
+    raise RuntimeError("Could not find a SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['34.197.126.49']
+ALLOWED_HOSTS = ['34.197.126.49'] if not debug else []
 
 
 # Application definition
@@ -126,8 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = f"/var/www/34.197.126.49/static"
+STATIC_URL = 'static/'
+
+STATIC_ROOT = f"/var/www/{ALLOWED_HOSTS[0]}/static" if not debug else None
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -137,7 +142,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = '/var/www/34.197.126.49/media/'#BASE_DIR / MEDIA_URL
+MEDIA_ROOT = f'/var/www/{ALLOWED_HOSTS[0]}/media/' if not debug else BASE_DIR / MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
